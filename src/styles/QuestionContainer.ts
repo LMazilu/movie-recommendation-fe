d1344bf0b3d5ac58b2dc7e106e4fcbf974e394ea
Liviu Marian Mazilu <liviumazilu79@gmail.com>
2024-06-26 19:57:44 +0200

    Added new base styles

---
+++ src/styles/QuestionContainer.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const QuestionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
+
+export default QuestionContainer;
