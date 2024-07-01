303faa32b97889fb86d46d85470b631a1d6f7d7e
Liviu Marian Mazilu <liviumazilu79@gmail.com>
2024-07-02 00:47:08 +0200

    Added styles and api

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Login from "./pages/LoginPage";
 import Register from "./pages/RegisterPage";
-import Questions from "./pages/QuestionsPage";
-import Years from "./pages/YearsPage";
-import Mood from "./pages/MoodPage";
-import QuestionPage2 from "./components/QuestionsPage";
+import Questions from "./components/QuestionsPage";
 import ThankYouPage from "./pages/ThankYouPage";
-import PlatformSelectionPage from "./pages/PlatformSelectionPage";
 import { FilmChoice } from "./pages/FilmChoice";
 
 const App: React.FC = () => {
@@ -19,9 +15,6 @@ const App: React.FC = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* <Route path="/questions" element={<Questions />} />
-        <Route path="/years" element={<Years />} />
-        <Route path="/mood" element={<Mood />} />
-        <Route path="/platform" element={<PlatformSelectionPage />} />
         <Route path="/thankyou" element={<ThankYouPage />} /> */}
 
         <Route path="/questions" element={<Questions />} />
